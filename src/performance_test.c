3d5a967ab1034d51f03cf96ba9060bd7fbf09700
Nola-Yang <yyyjiayue@outlook.com>
2026-01-29 12:05:51 -0500

    fix sync

--- src/performance_test.c
+++ src/performance_test.c
@@ -81,21 +81,23 @@ static void task_a_func() {
 	char sync_reply[SYNC_MSG_LEN];
 
 	uint64_t avg_time = 0;
-	uint32_t time = 0;
+	uint32_t start_time = 0;
+	uint32_t loop_overhead = 0;
 
 	for (uint32_t i = 0; i < MSG_TYPE_COUNT; ++i) {
 		msg_len = Msg_Lens[i];
 		reply_len = msg_len;
 		reply = Msgs[i];
 
+		shared_start_time = read_timer();
+
 		for (uint32_t j = 0; j < TEST_COUNT; ++j) {
-			shared_start_time = read_timer();
 			Receive(&sender_tid, msg, msg_len);
 			Reply(sender_tid, reply, reply_len);
-
-			// Sync: wait for task_b to read shared_start_time before next iteration
-			Send(TASK_B_TID, "sync", SYNC_MSG_LEN, sync_reply, SYNC_MSG_LEN);
 		}
+
+		// Sync: wait for task_b to finish timing before next msg_len
+		Send(TASK_B_TID, "sync", SYNC_MSG_LEN, sync_reply, SYNC_MSG_LEN);
 	}
 
 	// Phase Sync: wait for task_b to be ready for Send First phase
@@ -105,18 +107,23 @@ static void task_a_func() {
 	char reply_buf[MAX_MSG_LEN] = {0};
 
 	for (uint32_t i = 0; i < MSG_TYPE_COUNT; ++i) {
-		avg_time = 0;
 		msg_len = Msg_Lens[i];
 		reply_len = msg_len;
 		reply = Msgs[i];
 
-		for (uint32_t j = 0; j < TEST_COUNT; ++j) {
-			time = read_timer();
-			Send(TASK_B_TID, reply, msg_len, reply_buf, reply_len);
-			time = read_timer() - time;
-
-			avg_time += time;
+		// Void loop to measure loop overhead
+		start_time = read_timer();
+		for (volatile uint32_t j = 0; j < TEST_COUNT; ++j) {
+			// empty loop
 		}
+		loop_overhead = read_timer() - start_time;
+
+		// Actual test
+		start_time = read_timer();
+		for (uint32_t j = 0; j < TEST_COUNT; ++j) {
+			Send(TASK_B_TID, reply, msg_len, reply_buf, reply_len);
+		}
+		avg_time = read_timer() - start_time - loop_overhead;
 
 		avg_time /= TEST_COUNT;
 		print_csv_row(Sender_First, msg_len, avg_time);
@@ -140,23 +147,30 @@ static void task_b_func() {
 	char sync_reply[SYNC_MSG_LEN] = "ack";
 
 	uint64_t avg_time = 0;
-	uint32_t time = 0;
+	uint32_t start_time = 0;
+	uint32_t loop_overhead = 0;
 
 	for (uint32_t i = 0; i < MSG_TYPE_COUNT; ++i) {
-		avg_time = 0;
 		msg_len = Msg_Lens[i];
 		reply_len = msg_len;
 		msg = Msgs[i];
 
+		// Void loop to measure loop overhead
+		start_time = read_timer();
+		for (volatile uint32_t j = 0; j < TEST_COUNT; ++j) {
+			// empty loop
+		}
+		loop_overhead = read_timer() - start_time;
+
+		// Actual test
 		for (uint32_t j = 0; j < TEST_COUNT; ++j) {
 			Send(TASK_A_TID, msg, msg_len, reply, reply_len);
-			time = read_timer();
-
-			// Sync: wait for task_a to finish current iteration before reading shared_start_time
-			Receive(&sender_tid, sync_msg, SYNC_MSG_LEN);
-			avg_time += time - shared_start_time;
-			Reply(sender_tid, sync_reply, SYNC_MSG_LEN);
 		}
+		avg_time = read_timer() - shared_start_time - loop_overhead;
+
+		// Sync: wait for task_a before next msg_len
+		Receive(&sender_tid, sync_msg, SYNC_MSG_LEN);
+		Reply(sender_tid, sync_reply, SYNC_MSG_LEN);
 
 		avg_time /= TEST_COUNT;
 		print_csv_row(Receiver_First, msg_len, avg_time);
