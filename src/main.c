24b545a9992d1e71694627536273001ed1b7f5f3
Nola-Yang <yyyjiayue@outlook.com>
2026-02-03 23:45:14 -0500

    client task

--- src/main.c
+++ src/main.c
@@ -4,14 +4,13 @@
 #include "task_scheduler.h"
 #include "syscall.h"
 #include "uart.h"
-#include "util.h"
 #include "nameserver.h"
-#include "rps_server.h"
-#include "rps_client.h"
-#include "rps_test.h"
 #include "timer.h"
 #include "gic.h"
+#include "clock_server.h"
+#include "idle_task.h"
 #include <stdint.h>
+#include <stddef.h>
 
 
 static const uint32_t FIRST_USER_TASK_PRIORITY = 1;
@@ -23,33 +22,88 @@ static TaskDescriptor_t *raw_buffers[PRIORITY_LEVELS][RING_BUFFER_SIZE];
 static RingBuffer_t queues[PRIORITY_LEVELS];
 
 
-void first_user_task() {
-    int32_t tid;
+typedef struct {
+    int delay_interval;  // Ticks to delay each time
+    int num_delays;      // Number of delays to perform
+} ClientParams_t;
 
-    uart_printf(CONSOLE, "========================================\r\n");
+// Client task - delays n times for t ticks each, printing after each delay
+static void k3_client_task(void) {
+    int my_tid = MyTid();
+    int parent_tid = MyParentTid();
 
-    #ifndef MEASURE
-    uart_printf(CONSOLE, "K2 Test Suite\r\n");
-    #else
-    uart_printf(CONSOLE, "K2 Performance Test\r\n");
-    #endif
+    ClientParams_t params;
+    Send(parent_tid, NULL, 0, (char *)&params, sizeof(params));
 
-    uart_printf(CONSOLE, "========================================\r\n");
-
-    #ifndef MEASURE
-    tid = Create(NAMESERVER_PRIORITY, nameserver_task);
-
-    if (tid != NAMESERVER_TID) {
-        uart_printf(CONSOLE, "ERROR: NameServer tid=%d (expected %d)\r\n", tid, NAMESERVER_TID);
+    int clock_tid = WhoIs(CLOCK_SERVER_NAME);
+    if (clock_tid < 0) {
+        uart_printf(CONSOLE, "Client %d: Failed to find clock server\r\n", my_tid);
         Exit();
     }
 
-    uart_printf(CONSOLE, "Created NameServer, tid=%d\r\n", tid);
-    rps_test_run();
-    #else
-    (void)tid;  // suppress unused warning
-    perform_test_run();
-    #endif
+    for (int i = 0; i < params.num_delays; i++) {
+        Delay(clock_tid, params.delay_interval);
+        int current_tick = Time(clock_tid);
+        uart_printf(CONSOLE, "Client tid=%d, interval=%d, completed=%d/%d, tick=%d\r\n",
+                    my_tid, params.delay_interval, i + 1, params.num_delays, current_tick);
+    }
+
+    uart_printf(CONSOLE, "Client %d: Finished all delays\r\n", my_tid);
+    Exit();
+}
+
+
+void first_user_task(void) {
+    uart_printf(CONSOLE, "========================================\r\n");
+    uart_printf(CONSOLE, "K3 Clock Server Test\r\n");
+    uart_printf(CONSOLE, "========================================\r\n");
+
+    int ns_tid = Create(NAMESERVER_PRIORITY, nameserver_task);
+    if (ns_tid != NAMESERVER_TID) {
+        uart_printf(CONSOLE, "ERROR: NameServer tid=%d (expected %d)\r\n", ns_tid, NAMESERVER_TID);
+        Exit();
+    }
+    uart_printf(CONSOLE, "Created NameServer, tid=%d\r\n", ns_tid);
+
+    int clock_tid = Create(CLOCK_SERVER_PRIORITY, clock_server_task); // Create Clock Server
+    uart_printf(CONSOLE, "Created ClockServer, tid=%d\r\n", clock_tid);
+    int idle_tid = Create(IDLE_TASK_PRIORITY, idle_task);     // Create Idle Task
+    uart_printf(CONSOLE, "Created IdleTask, tid=%d\r\n", idle_tid);
+
+    // Create 4 client tasks with different priorities
+    int client1 = Create(K3_CLIENT_PRIORITY_3, k3_client_task);
+    int client2 = Create(K3_CLIENT_PRIORITY_4, k3_client_task);
+    int client3 = Create(K3_CLIENT_PRIORITY_5, k3_client_task);
+    int client4 = Create(K3_CLIENT_PRIORITY_6, k3_client_task);
+
+    uart_printf(CONSOLE, "Created clients: %d, %d, %d, %d\r\n",
+                client1, client2, client3, client4);
+
+    ClientParams_t params[4] = {
+        { .delay_interval = 10, .num_delays = 20 },  // Priority 3
+        { .delay_interval = 23, .num_delays = 9 },   // Priority 4
+        { .delay_interval = 33, .num_delays = 6 },   // Priority 5
+        { .delay_interval = 71, .num_delays = 3 },   // Priority 6
+    };
+
+    // Receive parameter requests from each client and reply
+    for (int i = 0; i < 4; i++) {
+        int sender_tid;
+        Receive(&sender_tid, NULL, 0);
+
+        int param_index;
+        if (sender_tid == client1) param_index = 0;
+        else if (sender_tid == client2) param_index = 1;
+        else if (sender_tid == client3) param_index = 2;
+        else param_index = 3;
+
+        Reply(sender_tid, (const char *)&params[param_index], sizeof(ClientParams_t));
+        uart_printf(CONSOLE, "Sent params to client %d: interval=%d, num=%d\r\n",
+                    sender_tid, params[param_index].delay_interval,
+                    params[param_index].num_delays);
+    }
+
+    uart_printf(CONSOLE, "FirstUserTask: All clients started, waiting for completion\r\n");
 
     Exit();
 }
@@ -75,11 +129,6 @@ int kmain() {
 	init_global_task_manager(tasks);
 
 	init_interrupts();
-
-	// Set initial timer compare values 
-	uint64_t current_time = read_timer();
-	timer_set_c1((uint32_t)(current_time + 10000));
-	timer_set_c3((uint32_t)(current_time + 10000));
 
 	kern_Create(FIRST_USER_TASK_PRIORITY, first_user_task);
 
