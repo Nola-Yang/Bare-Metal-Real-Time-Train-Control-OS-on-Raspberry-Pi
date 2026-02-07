f16a722061f20e9e3b9358eb8e382a5a21ecb0ea
Nola-Yang <yyyjiayue@outlook.com>
2026-02-07 12:29:32 -0500

    some preliminary design

--- include/uart.h
+++ include/uart.h
@@ -2,8 +2,13 @@
 #define _uart_h_ 1
 
 #include <stddef.h>
+#include <stdint.h>
 
 #define CONSOLE 0
+
+// UART interrupt bits
+#define UART_INT_RX  (1 << 4)
+#define UART_INT_TX  (1 << 5)
 
 void uart_config_and_enable(size_t line);
 char uart_getc(size_t line);
@@ -13,5 +18,20 @@ void uart_puts(size_t line, const char *buf);
 void uart_printf(size_t line, const char *fmt, ...);
 
 void uart_debug_printf(size_t line, const char *fmt, ... );
+
+// Interrupt control functions
+void uart_enable_rx_interrupt(size_t line);
+void uart_disable_rx_interrupt(size_t line);
+void uart_enable_tx_interrupt(size_t line);
+void uart_disable_tx_interrupt(size_t line);
+void uart_clear_rx_interrupt(size_t line);
+void uart_clear_tx_interrupt(size_t line);
+uint32_t uart_read_mis(size_t line);
+
+// Non-blocking I/O
+int uart_rx_ready(size_t line);
+int uart_tx_ready(size_t line);
+int uart_getc_nonblocking(size_t line);
+int uart_putc_nonblocking(size_t line, char c);
 
 #endif /* uart.h */
