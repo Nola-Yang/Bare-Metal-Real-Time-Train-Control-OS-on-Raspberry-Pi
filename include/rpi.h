ac868e609799dfba6ea693a8db16421e54ad0b99
Nola-Yang <yyyjiayue@outlook.com>
2026-02-11 18:03:36 -0500

    can server stuff

--- include/rpi.h
+++ include/rpi.h
@@ -8,9 +8,11 @@ static char* const MMIO_BASE = (char*)0xFE000000;
 void gpio_init(void);
 
 // GPIO interrupt functions for MCP2515
-void gpio_enable_falling_edge_detect(uint32_t pin);
-void gpio_clear_event(uint32_t pin);
-uint32_t gpio_check_event(uint32_t pin);
+void gpio_init_interrupt(void);
+uint32_t gpio_get_event_detect_status(uint32_t pin);
+void gpio_clr_event_detect_status(uint32_t pin);
+void gpio_enable_can_interrupt(void);
+void gpio_disable_can_interrupt(void);
 
 // Get event detect status for GPIO pin.
 uint32_t gpio_get_event_detect_status(uint32_t pin);
