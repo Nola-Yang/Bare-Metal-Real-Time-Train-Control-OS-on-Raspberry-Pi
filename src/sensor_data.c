d2fc4399849f85e8f612140ccdd5a395bbc42046
Nola-Yang <yyyjiayue@outlook.com>
2026-02-12 10:11:29 -0500

    remove

--- src/sensor_data.c
+++
@@ -1,9 +0,0 @@
-#include "sensor_data.h"
-
-
-void init_sensor_data(SensorData * sensor_data, char bank, uint32_t sensor_no, bool old_state, bool new_state) {
-    sensor_data->bank = bank;
-    sensor_data->sensor_no = sensor_no;
-    sensor_data->old_state = old_state;
-    sensor_data->new_state = new_state;
-}
