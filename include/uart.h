793d1ec654574f4236ba23773c80b6db34be6996
Nola-Yang <yyyjiayue@outlook.com>
2026-02-11 21:27:03 -0500

    add error interrupt handle and en/disable setting

--- include/uart.h
+++ include/uart.h
@@ -9,6 +9,19 @@
 // UART interrupt bits
 #define UART_INT_RX  (1 << 4)
 #define UART_INT_TX  (1 << 5)
+#define UART_INT_RT  (1 << 6)  // RX timeout
+#define UART_INT_FE  (1 << 7)  // Framing error
+#define UART_INT_PE  (1 << 8)  // Parity error
+#define UART_INT_BE  (1 << 9)  // Break error
+#define UART_INT_OE  (1 << 10) // Overrun error
+// Modem/status interrupts (PL011)
+#define UART_INT_DSR (1 << 3)
+#define UART_INT_DCD (1 << 2)
+#define UART_INT_CTS (1 << 1)
+#define UART_INT_RI  (1 << 0)
+
+#define UART_INT_ERROR_MASK  (UART_INT_FE | UART_INT_PE | UART_INT_BE | UART_INT_OE)
+#define UART_INT_STATUS_MASK (UART_INT_DSR | UART_INT_DCD | UART_INT_CTS | UART_INT_RI)
 
 void uart_config_and_enable(size_t line);
 char uart_getc(size_t line);
@@ -29,6 +42,12 @@ void uart_enable_tx_interrupt(size_t line);
 void uart_disable_tx_interrupt(size_t line);
 void uart_clear_rx_interrupt(size_t line);
 void uart_clear_tx_interrupt(size_t line);
+void uart_enable_error_interrupts(size_t line);
+void uart_disable_error_interrupts(size_t line);
+void uart_clear_error_interrupts(size_t line);
+void uart_enable_status_interrupts(size_t line);
+void uart_disable_status_interrupts(size_t line);
+void uart_clear_status_interrupts(size_t line);
 uint32_t uart_read_mis(size_t line);
 
 // Non-blocking I/O
@@ -36,5 +55,6 @@ int uart_rx_ready(size_t line);
 int uart_tx_ready(size_t line);
 int uart_getc_nonblocking(size_t line);
 int uart_putc_nonblocking(size_t line, char c);
+int uart_cts_ready(size_t line);
 
 #endif /* uart.h */
