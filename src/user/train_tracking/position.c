9841a711b71a50afb9a29c9b8b550a7e81163730
Alex Au <a32au@uwaterloo.ca>
2026-03-03 08:14:35 -0500

    Temporarily change speeds for Track B

--- src/user/train_tracking/position.c
+++ src/user/train_tracking/position.c
@@ -270,7 +270,7 @@ void transition_to_enter_loop(train_pos_t *pos, uint64_t now_us) {
 #ifdef TRACK_A
     static int32_t SPEED_OVERRIDES[MAX_PHYSICAL_TRAINS] = {227, 232, 242, 229, 230};
 #else
-    static int32_t SPEED_OVERRIDES[MAX_PHYSICAL_TRAINS] = {280, 280, 280, 280, 280};
+    static int32_t SPEED_OVERRIDES[MAX_PHYSICAL_TRAINS] = {227, 227, 242, 229, 230};
 #endif
 
 /*
