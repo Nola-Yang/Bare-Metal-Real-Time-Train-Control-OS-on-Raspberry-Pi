97450d7f5352370e3fbb35c7cf066ba23908cfeb
Nola-Yang <yyyjiayue@outlook.com>
2026-03-06 15:09:51 -0500

    fix rv delay

--- src/user/train_control.c
+++ src/user/train_control.c
@@ -256,7 +256,13 @@ void train_control_task(void) {
                     train = -1;
                 }
                 reply.train = train;
-                reply.delay_ticks = 100;  // 100 ticks * 10ms = 1s
+                int speed_level = 0;
+                if (train >= 0) {
+                    train_pos_t *pos = pos_get(train);
+                    if (pos) speed_level = pos->user_speed;
+                }
+                // delay_us = 6000 * speed_level (0-14); convert to 10ms ticks
+                reply.delay_ticks = (6000 * speed_level) / 10000;
                 Reply(tid, (const char *)&reply, sizeof(reply));
                 break;
             }
