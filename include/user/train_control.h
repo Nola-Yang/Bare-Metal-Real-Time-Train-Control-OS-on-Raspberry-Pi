48102fac1fda0fc9e3f46e2c752db87469982718
Nola-Yang <yyyjiayue@outlook.com>
2026-03-19 22:59:59 -0400

    split ui from train control

--- include/user/train_control.h
+++ include/user/train_control.h
@@ -7,7 +7,6 @@
 // Message types for train control task
 #define TRAIN_MSG_CHAR      0   // Keyboard character input
 #define TRAIN_MSG_CAN_FRAME 1   // CAN frame received
-#define TRAIN_MSG_TICK      2   // Periodic UI tick
 #define TRAIN_MSG_RV_REQUEST  3   // Reverse delay task requests parameters
 #define TRAIN_MSG_RV_COMPLETE 4
 #define TRAIN_MSG_DEMO_TICK   5   // 1s periodic demo tick
@@ -36,7 +35,7 @@ void train_control_task(void);
 // Keyboard courier task
 void keyboard_courier_task(void);
 
-// UI tick task
+// Periodic UI refresh task
 void ui_tick_task(void);
 
 // Demo tick task (1s interval)
--- src/user/train_control.c
+++ src/user/train_control.c
@@ -25,6 +25,7 @@ RING_BUFFER_DECLARE(DeadTrainQueue_t, int, MAX_ACTIVE_TRAINS + 1);
 static DeadTrainQueue_t DeadTrainQueue;
 
 static int Train_Nums[MAX_ACTIVE_TRAINS] = {13, 14, 15, 17, 18};
+static uint64_t ui_start_us;
 
 void rv_delay_task(void) {
     int parent = MyParentTid();
@@ -86,21 +87,39 @@ void demo_tick_task(void) {
 }
 
 void ui_tick_task(void) {
-    int parent = MyParentTid();
     int clock_tid = WhoIs(CLOCK_SERVER_NAME);
 
     KASSERT(clock_tid >= 0);
-
-    TrainControlMsg_t msg;
-    TrainControlReply_t reply;
-    msg.type = TRAIN_MSG_TICK;
 
     const int tick_interval = 4;  // 4 ticks * 10ms = 40ms
 
     for (;;) {
         Delay(clock_tid, tick_interval);
-        Send(parent, (const char *)&msg, sizeof(msg),
-             (char *)&reply, sizeof(reply));
+
+        uint64_t tick_now = read_timer();
+        ui_update_clock(ui_start_us, tick_now);
+        ui_update_idle(get_idle_percentage());
+
+        if (ui_is_switches_dirty()) {
+            ui_puts("\033[s");
+            ui_switches();
+            ui_puts("\033[u");
+            ui_mark_switches_clean();
+        }
+        if (ui_is_sensors_dirty()) {
+            ui_puts("\033[s");
+            ui_draw_sensors(ui_start_us);
+            ui_puts("\033[u");
+            ui_mark_sensors_clean();
+        }
+        if (ui_is_position_dirty()) {
+            ui_puts("\033[s");
+            ui_draw_position();
+            ui_draw_prediction_error();
+            ui_draw_offroute();
+            ui_puts("\033[u");
+            ui_mark_position_clean();
+        }
     }
 }
 
@@ -276,7 +295,7 @@ void train_control_task(void) {
     int cmdlen = 0;
     int rv_pending_count = 0;  /* # rv still in progress */
 
-    uint64_t start_us = read_timer();
+    ui_start_us = read_timer();
     int running = 1;
 
     Putc(term_tid, TERM_CHANNEL_CONSOLE, '\0');  
@@ -353,39 +372,6 @@ void train_control_task(void) {
                 Reply(tid, (const char *)&reply, sizeof(reply));
                 uint64_t tick_now = read_timer();
                 pos_on_switch_settle_tick(tick_now);
-                break;
-            }
-
-            case TRAIN_MSG_TICK: {
-                // Periodic UI updates
-                Reply(tid, (const char *)&reply, sizeof(reply));
-
-                uint64_t tick_now = read_timer();
-                ui_update_clock(start_us, tick_now);
-
-                int idle_percent = get_idle_percentage();
-                ui_update_idle(idle_percent);
-
-                if (ui_is_switches_dirty()) {
-                    ui_puts("\033[s");
-                    ui_switches();
-                    ui_puts("\033[u");
-                    ui_mark_switches_clean();
-                }
-                if (ui_is_sensors_dirty()) {
-                    ui_puts("\033[s");
-                    ui_draw_sensors(start_us);
-                    ui_puts("\033[u");
-                    ui_mark_sensors_clean();
-                }
-                if (ui_is_position_dirty()) {
-                    ui_puts("\033[s");
-                    ui_draw_position();
-                    ui_draw_prediction_error();
-                    ui_draw_offroute();
-                    ui_puts("\033[u");
-                    ui_mark_position_clean();
-                }
                 break;
             }
 
